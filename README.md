a2e49b0010a895dd9fdbbd24bcfef7a09325a11a
hui <huiqin33333@outlook.com>
2019-04-03 05:49:55 -0700

    Complete test project

---
+++ README.md
@@ -0,0 +1,25 @@
+## 1. Setup
+```bash
+npm install
+```
+- install all npm and bower dependencies
+
+## 2. Watch files
+```bash
+npm start
+```
+or
+```bash
+gulp
+```
+
+- all SCSS/HTML will be watched for changes and injected into browser thanks to BrowserSync
+
+## 3. Build production version
+```bash
+npm run build
+```
+or
+```bash
+gulp build
+```
---
+++ app/controller.js
@@ -0,0 +1,22 @@
+/**
+ * Main application controller
+ *
+ * You can use this controller for your whole app if it is small
+ * or you can have separate controllers for each logical section
+ * 
+ */
+;(function() {
+
+  angular
+    .module('boilerplate')
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['LocalStorage', 'QueryService'];
+
+
+  function MainController(LocalStorage, QueryService) {
+    var self = this;
+  }
+
+
+})();
